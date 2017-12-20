0d38ea1d90daec1607e07d148c924b23d7d40114
Eric Robibaro <eric.robibaro@radialpoint.com>
2017-12-20 16:22:03 -0500

    Initial version

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dot4db',
+    version='',
+    packages=[''],
+    url='https://github.com/perlchild/dot4db',
+    license='Apache License 2.0',
+    author='Eric Robibaro',
+    author_email='perlchild@perlchild.org',
+    description=''
+)
